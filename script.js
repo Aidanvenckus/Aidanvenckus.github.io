64c45612a35bccfadd852f3af91aa98cb4e7f822
Aidan Venckus <avenckus@ucsc.edu>
2021-07-09 18:53:51 -0700

    Adding a new navigation bar

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+//Importing the 2 elements
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+//On click of toggle button
+toggleButton.addEventListener('click', () => {
+    //if there is an active then remove it and if there isnt one then add it
+    navbarLinks.classList.toggle('active')
+})
