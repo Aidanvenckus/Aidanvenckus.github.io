9f1d639598f8355a322e78193b0fed3427934b6f
Aidan Venckus <avenckus@ucsc.edu>
2021-07-09 19:04:01 -0700

    test

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-//On click of toggle button
+//On click of toggle button run a function listed underneath
 toggleButton.addEventListener('click', () => {
     //if there is an active then remove it and if there isnt one then add it
     navbarLinks.classList.toggle('active')
